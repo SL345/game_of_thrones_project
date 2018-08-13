07ffbe192bf50c70815c89d0eb47a899504b861e
SL345 <sherilyn345@gmail.com>
2018-08-13 07:46:28 +0000

    Done

--- q02_eda_major_death/build.py
+++ q02_eda_major_death/build.py
@@ -1,5 +1,7 @@
+# %load q02_eda_major_death/build.py
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.game_of_thrones.q01_feature_engineering.build import q01_feature_engineering
@@ -7,8 +9,10 @@ from greyatomlib.game_of_thrones.q01_feature_engineering.build import q01_featur
 battles = pd.read_csv('data/battles.csv')
 character_predictions = pd.read_csv('data/character-predictions.csv')
 
-def q02_eda_major_death():
-    "write your solution here"
+def q02_eda_major_death(battles):
+    'write your solution here'
+    battles.groupby('year').sum()[['major_death','major_capture']].plot.bar()
+
 
 
 
