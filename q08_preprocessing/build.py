3c829df873c549c668d0ec97490e358556db7ab5
SL345 <sherilyn345@gmail.com>
2018-08-14 04:43:42 +0000

    Done

--- q08_preprocessing/build.py
+++ q08_preprocessing/build.py
@@ -1,3 +1,4 @@
+# %load q08_preprocessing/build.py
 import pandas as pd
 import numpy as np
 import sys,os
@@ -10,7 +11,23 @@ character_predictions = pd.read_csv('data/character-predictions.csv')
 
 
 
-def q08_preprocessing():
-    "write your solution here"
+def q08_preprocessing(character_predictions):
+    'write your solution here'
+    copy_df = character_predictions.copy(deep=True)
+
+    copy_df.loc[:,['culture']] = [q07_culture_survival(x) for x in copy_df.culture.fillna('')]
+    copy_df['title'] =pd.factorize(copy_df['title'])[0]
+    copy_df['culture'] =pd.factorize(copy_df['culture'])[0]
+    copy_df['mother'] =pd.factorize(copy_df['mother'])[0]
+    copy_df['father'] =pd.factorize(copy_df['father'])[0]
+    copy_df['house'] =pd.factorize(copy_df['house'])[0]
+    copy_df['heir'] =pd.factorize(copy_df['heir'])[0]
+    copy_df['spouse'] =pd.factorize(copy_df['spouse'])[0]
+    copy_df.drop(['name', 'alive', 'pred', 'plod', 'isAlive', 'dateOfBirth'],1,inplace=True)
+    copy_df.replace('.',' ',inplace=True)
+    copy_df.replace('_',' ',inplace=True)
+    copy_df.fillna(-1,inplace=True)
+    return copy_df
+
 
 
